93360b4d680a78cf794d16047d719603973a0f90
izkyto <ngonzalez@mobydigital.com>
2022-01-19 14:36:18 -0300

    styles changes

--- components/Guitarra.jsx
+++ components/Guitarra.jsx
@@ -4,17 +4,13 @@ import styles from '../styles/Guitarra.module.css'
 
 
 const Guitarra = ({ guitarra }) => {
-    const { nombre, url, descripcion, precio, imagen } = guitarra
-
-    const imagenUrl = imagen ? imagen.url : '/img/nodisponible.png'
+    const { nombre, url, descripcion, precio, imagen } = guitarra    
+    const imagenUrl = imagen ? imagen.url : '/img/nodisponible.png'    
 
     return (
         <div className={styles.guitarra}>
-            {imagen
-                ? <Image layout='responsive' height={350} width={180} src={imagenUrl} alt={`Imagen de ${nombre}`} ></Image>
-                : <p className={styles.descripcion}>imagen no disponible</p>
-            }
 
+            <Image layout='responsive' height={350} width={180} src={imagenUrl} alt={`Imagen de ${nombre}`} ></Image>
             <div className={styles.contenido}>
                 <h3>{nombre}</h3>
                 <p className={styles.descripcion}>{descripcion}</p>
